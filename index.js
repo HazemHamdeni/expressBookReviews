fda58232e0886b175dc554ebd36e7af9756aa15e
Hazem HAMDENI <h.hamdeni@sofrecom-tunisie.com>
2023-10-04 15:04:05 +0100

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req,res,next){
+if (req.session && req.session) {
+    try {
+    const decodedToken = jwt.verify(req.session.accessToken,'HazemHAMDENI');
+    next();
+}  catch (error) {
+    res.status(403).json({error:'Access Denied.'})
+}
+} else {
+    res.status(401).json ({error : 'Unauthorized.'})
+}
+});
+ 
+const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
